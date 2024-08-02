71a4da0eacbdb9d3493e8da7c5a8eef75702bf15
Malekabo123 <malismail20@posta.pau.edu.tr>
2024-08-02 09:59:48 +0300

    securith

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBIPCJuhNNRkdO-5MouylEU9LKlS0fhWhc',
-    authDomain: 'leave-management-a3f46.firebaseapp.com',
-    projectId: 'leave-management-a3f46',
-    storageBucket: 'leave-management-a3f46.appspot.com',
-    messagingSenderId: '746924621999',
-    appId: '1:746924621999:web:6cc1d41640466cdb7f545a',
+    apiKey: '***',
+    authDomain: '***',
+    projectId: '***',
+    storageBucket: '***.appspot.com',
+    messagingSenderId: '*****',
+    appId: '**********',
   },
 };
